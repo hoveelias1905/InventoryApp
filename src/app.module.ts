7d198e67cb18597e532d122cbb47efce97a9dab2
hoveelias1905 <hoveelias1905@gmail.com>
2026-07-03 10:35:04 +0200

    Modules Done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { StoresModule } from './stores/stores.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, ProductsModule, StoresModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
